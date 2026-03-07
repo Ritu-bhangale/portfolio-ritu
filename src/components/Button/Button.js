a1a6a7ab414edf74ad76b153c10696639f0e494a
Ritu Bhangale <ritusbhangale@gmail.com>
2026-03-07 18:53:33 +0530

    Restore Button component files to pre-c25d49b state

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,14 +1,10 @@
-import flowerIcon from 'assets/flower.svg';
 import { Icon } from 'components/Icon';
 import { Loader } from 'components/Loader';
 import { Transition } from 'components/Transition';
-import { useAppContext } from 'hooks';
 import RouterLink from 'next/link';
-import { forwardRef, useEffect, useMemo, useRef, useState } from 'react';
+import { forwardRef } from 'react';
 import { classes } from 'utils/style';
 import styles from './Button.module.css';
-
-const SEASONS = ['summer', 'monsoon', 'autumn', 'winter'];
 
 function isExternalLink(href) {
   return href?.includes('://');
@@ -44,33 +40,17 @@ const ButtonContent = forwardRef(
       target,
       href,
       disabled,
-      onClick,
-      onMouseEnter,
-      onMouseLeave,
       ...rest
     },
     ref
   ) => {
-    const { seasonIndex } = useAppContext();
     const isExternal = isExternalLink(href);
     const defaultComponent = href ? 'a' : 'button';
     const Component = as || defaultComponent;
-    const [flowerSpin, setFlowerSpin] = useState(false);
-    const [sunBurstActive, setSunBurstActive] = useState(false);
-    const timeoutsRef = useRef([]);
-    const season = useMemo(() => SEASONS[seasonIndex ?? 0] || SEASONS[0], [seasonIndex]);
-    const summerEnabled = season === 'summer' && !!iconEnd;
-
-    useEffect(() => {
-      return () => {
-        const timeouts = timeoutsRef.current;
-        timeouts.forEach(timeout => window.clearTimeout(timeout));
-      };
-    }, []);
 
     return (
       <Component
-        className={classes(styles.button, 'season-button', summerEnabled && styles.summerEnabled, className)}
+        className={classes(styles.button, className)}
         data-loading={loading}
         data-icon-only={iconOnly}
         data-secondary={secondary}
@@ -81,24 +61,6 @@ const ButtonContent = forwardRef(
         target={target || isExternal ? '_blank' : undefined}
         disabled={disabled}
         ref={ref}
-        onMouseEnter={event => {
-          onMouseEnter?.(event);
-        }}
-        onMouseLeave={event => {
-          onMouseLeave?.(event);
-        }}
-        onClick={event => {
-          onClick?.(event);
-
-          if (!summerEnabled) return;
-
-          setFlowerSpin(true);
-          setSunBurstActive(true);
-
-          const spinTimeout = window.setTimeout(() => setFlowerSpin(false), 460);
-          const burstTimeout = window.setTimeout(() => setSunBurstActive(false), 520);
-          timeoutsRef.current.push(spinTimeout, burstTimeout);
-        }}
         {...rest}
       >
         {!!icon && (
@@ -109,33 +71,14 @@ const ButtonContent = forwardRef(
             icon={icon}
           />
         )}
-        {!!children && <span className={classes(styles.text, 'label')}>{children}</span>}
+        {!!children && <span className={styles.text}>{children}</span>}
         {!!iconEnd && (
-          <span className={summerEnabled ? styles.endIconStack : undefined}>
-            <Icon
-              className={classes(styles.icon, summerEnabled && styles.arrowIcon)}
-              data-end={!iconOnly}
-              data-shift={iconHoverShift}
-              icon={iconEnd}
-            />
-            {summerEnabled && (
-              <>
-                <img
-                  className={classes(
-                    styles.icon,
-                    styles.flowerIcon,
-                    flowerSpin && styles.flowerSpin
-                  )}
-                  src={flowerIcon.src || flowerIcon}
-                  alt=""
-                />
-                <span
-                  className={classes(styles.sunBurst, sunBurstActive && styles.sunBurstActive)}
-                  aria-hidden
-                />
-              </>
-            )}
-          </span>
+          <Icon
+            className={styles.icon}
+            data-end={!iconOnly}
+            data-shift={iconHoverShift}
+            icon={iconEnd}
+          />
         )}
         <Transition unmount in={loading}>
           {visible => (
